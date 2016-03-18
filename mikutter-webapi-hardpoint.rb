7456dbf4dbb134d8c7d1a68df258cb8e4f82e097
moguno <moguno@0kn.sakura.ne.jp>
2016-03-19 07:46:58 +0900

    mikutterを多重起動できなくなる問題を解決

--- mikutter-webapi-hardpoint.rb
+++ mikutter-webapi-hardpoint.rb
@@ -3,16 +3,22 @@
 Plugin.create(:"mikutter-webapi-hardpoint") {
   require "webrick"
 
-  @server = WEBrick::HTTPServer.new({ :BindAddress => "127.0.0.1", :Port => 39080 })
-  
   # APIを登録する
   defdsl(:webapi) { |endpoint, &handler|
-    @server.mount_proc("/#{endpoint}", &handler)
+    if @server
+      @server.mount_proc("/#{endpoint}", &handler)
+    end
   }
 
   # プラグインロード時
   on_boot { |service|
     if service == Service.primary 
+      begin
+        @server = WEBrick::HTTPServer.new({ :BindAddress => "127.0.0.1", :Port => 39080 })
+      rescue => e
+        activity(:system, _("WebAPI用のWebサーバの起動に失敗しました"))
+	next
+      end
 
       # 指定されたタイトルとURLをポストボックスに突っ込む
       webapi("test") { |req, res|
